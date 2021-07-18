0fe6158dce851c5add6965f01e3e31d981d68c65
Yuji Sode <yuji.sode@gmail.com>
2021-07-18 19:51:44 +0900

    Add files via upload

---
+++ sample01/ndrandom_sample01.tcl
@@ -0,0 +1,66 @@
+#ndrandom_sample01.tcl
+#=====================
+#
+#u=[0,100] and du=1000
+#A,B and C=u
+#probability p=p(A<B&&C<B)
+#=====================
+#
+source -encoding utf-8 nDRandom.tcl;
+#
+#loading data range, and output generator
+::nDRandom::setRange 0 100 1000;
+set filename [::nDRandom::output sample01 tcl];
+puts stdout "\#filename: $filename";
+#
+source -encoding utf-8 $filename;
+puts stdout [expr {sample01_info()}];
+#=====================
+#
+#---------
+set u {};
+set A 0.0;
+set B 0.0;
+set C 0.0;
+set p 0.0;
+#---------
+#
+#---------
+#number of sample size
+set sampleSize 1000;
+#
+#number of sample set
+set sampleSet 10;
+#---------
+#
+set i 0;
+set iSet 0;
+set R 0.0;
+#
+while {$iSet<$sampleSet} {
+	#
+	set i 0;
+	set R [expr {rand()}];
+	#
+	while {$i<$sampleSize} {
+		#
+		set u [expr {sample01_random(rand())}];
+		set A [expr {double([lindex $u 0])+$R*double([lindex $u 1])}];
+		#---------
+		set u [expr {sample01_random(rand())}];
+		set B [expr {double([lindex $u 0])+$R*double([lindex $u 1])}];
+		#---------
+		set u [expr {sample01_random(rand())}];
+		set C [expr {double([lindex $u 0])+$R*double([lindex $u 1])}];
+		#---------
+		#
+		set p [expr {$A<$B&&$C<$B?$p+1.0:$p}];
+		#
+		incr i 1;
+	};
+	#
+	incr iSet 1;
+};
+#
+puts stdout "p(A<B&&C<B) = [expr {$p/double($sampleSize*$sampleSet)}]";
+puts stdout "sample: [format %e $sampleSize] x $sampleSet sets";
---
+++ sample01/sample01.tcl
@@ -0,0 +1,14 @@
+#timestamp:Sun Jul 18 13:33:58 JST 2021
+#ID MIN MAX divisions dx
+#x1 0 100 1000 0.1
+#it returns preset data
+proc ::tcl::mathfunc::sample01_info {} {return {ID MIN MAX divisions dx
+x1 0 100 1000 0.1};};
+#it returns a random coordinates in n-th dimensions
+# - $double: a real number in range (0.0, 1.0)
+#output coordinates: {x1 dx1 x2 dx2 ... xn dxn} where Xi = xi+k*dxi and k is a constant
+proc ::tcl::mathfunc::sample01_random {double} {
+	set result {};set idx 0;set block 0;set u [expr {$double<0?0.0:$double}];set u [expr {$double>1?1.0:$double}];set u [expr {int(floor($u*1000))}];
+	set idx [expr {$u%1000}];set block [expr {int(ceil($u/1000.0))}];lappend result [expr {0.0+0.1*$idx}];lappend result 0.1;
+	unset idx block;return $result;
+};
