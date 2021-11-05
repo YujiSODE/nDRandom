8ee114c26c6a2837df7c5c251fc28cf2bb8b6adb
Yuji Sode <yuji.sode@gmail.com>
2021-11-05 11:45:17 +0900

    Add files via upload

--- sample02/sample02.tcl
+++ sample02/sample02.tcl
@@ -1,4 +1,4 @@
-#timestamp:Fri Jul 23 16:29:30 JST 2021
+#timestamp:Fri Nov 05 11:43:57 JST 2021
 #ID MIN MAX divisions dx
 #x1 0 100 1000 0.1
 #x2 0 100 1000 0.1
@@ -13,8 +13,8 @@ x3 0 100 1000 0.1};};
 #output coordinates: {x1 dx1 x2 dx2 ... xn dxn} where Xi = xi+c*dxi and c is a constant
 proc ::tcl::mathfunc::sample02_random {double} {
 	set result {};set idx 0;set block 0;set u [expr {$double<0?0.0:$double}];set u [expr {$double>1?1.0:$double}];set u [expr {int(floor($u*1000000000))}];
-	set idx [expr {$u%1000}];set block [expr {int(ceil($u/1000.0))}];lappend result [expr {0.0+0.1*$idx}];lappend result 0.1;
-	set idx [expr {$block%1000}];set block [expr {int(ceil($u/1000000.0))}];lappend result [expr {0.0+0.1*$idx}];lappend result 0.1;
-	set idx [expr {$block%1000}];set block [expr {int(ceil($u/1000000000.0))}];lappend result [expr {0.0+0.1*$idx}];lappend result 0.1;
+	set idx [expr {$u%1000}];set block [expr {int(floor($u/1000.0))}];lappend result [expr {0.0+0.1*$idx}];lappend result 0.1;
+	set idx [expr {$block%1000}];set block [expr {int(floor($u/1000000.0))}];lappend result [expr {0.0+0.1*$idx}];lappend result 0.1;
+	set idx [expr {$block%1000}];set block [expr {int(floor($u/1000000000.0))}];lappend result [expr {0.0+0.1*$idx}];lappend result 0.1;
 	unset u idx block;return $result;
 };
